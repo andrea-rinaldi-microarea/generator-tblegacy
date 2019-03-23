7ead133b1d06a864f0c7d32e49faba0e3ccbb6e4
daaren-urthling <daaren.urthling@hotmail.com>
2019-03-23 16:58:26 +0100

    document master/detail (to be tested)

--- generators/document/index.js
+++ generators/document/index.js
@@ -252,6 +252,16 @@ module.exports = class extends Generator {
             this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\_IDD_VIEW.tbjson'),
             this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\IDD_' + _.toUpper(this.properties.documentName) + '_VIEW.tbjson')
         );
+        if (this.properties.documentType === MASTER_DETAIL) {
+            this.fs.move(
+                this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\_IDD_DETAIL.hjson'),
+                this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\IDD_' + _.toUpper(this.properties.documentName) + '_DETAIL.hjson')
+            );
+            this.fs.move(
+                this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\_IDD_DETAIL.tbjson'),
+                this.modulePath('ModuleObjects\\' + this.properties.documentName + '\\JsonForms\\IDD_' + _.toUpper(this.properties.documentName) + '_DETAIL.tbjson')
+            );
+        }
 
         this.fs.copy(
             this.modulePath('ModuleObjects\\DocumentObjects.xml'),
