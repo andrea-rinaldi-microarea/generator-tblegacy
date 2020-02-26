c498fa1bd17112c057c6a3559fdefd97b232120e
LanderCC <61467470+LanderCC@users.noreply.github.com>
2020-02-26 15:59:05 +0100

    Update File _adm.h
    It gives an error while creating a document in this file: Undefined identifier.

--- generators/document/templates/_components/_adm.h
+++ generators/document/templates/_components/_adm.h
@@ -16,9 +16,9 @@ class TB_EXPORT ADM<%= documentName %>Obj : public ADMObj
 public:
 	virtual	ADMObj*				GetADM					()					= 0;
 	virtual	<%= tableName %>*	Get<%= tableBaseName %>	()			const	= 0;
-<% if (documentType === MASTER_DETAIL) { -%>	
-	virtual	<%= tableName %>Details*	GetDetail	(int nRow)		const	= 0;
-<% } -%>
+	if (documentType === MASTER_DETAIL) {	
+		virtual	<%= tableName %>Details*	GetDetail	(int nRow)		const	= 0;
+	}
 };
 
 #include "endh.dex"
